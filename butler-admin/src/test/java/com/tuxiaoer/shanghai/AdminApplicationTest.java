5c5ae7c3eaa1922539eb4dea76434f3c02edb16b
YeJR <2549908936@qq.com>
2019-05-28 15:34:21 +0800

    修改注册中心配置

---
+++ butler-admin/src/test/java/com/tuxiaoer/shanghai/AdminApplicationTest.java
@@ -0,0 +1,21 @@
+package com.tuxiaoer.shanghai;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author ：YeJR
+ * @version : 1.0
+ * @date ：2019/4/9 14:06
+ * @description：测试类
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminApplicationTest {
+
+    public void test() {
+
+    }
+
+}
